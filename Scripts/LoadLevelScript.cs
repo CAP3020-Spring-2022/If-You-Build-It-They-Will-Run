b4aaa5f5505e54fe84c78cd4fe0e1195c51f5202
erinhu00 <58484479+erinhu00@users.noreply.github.com>
2022-03-05 21:43:05 -0500

    MainMenu

---
+++ Scripts/LoadLevelScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadLevelScript : MonoBehaviour {
+//Attach this script to the GameManager and use it to load the other scene
+
+	public int levelToLoad;
+
+	public void ApplicationLoadLevel(){
+		Application.LoadLevel (levelToLoad);
+	}
+}
