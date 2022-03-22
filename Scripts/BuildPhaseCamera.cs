b38aab0d6f84d7c18134215bcef4181679c104a9
BlakeOneal <blakeoneal@ufl.edu>
2022-03-22 16:29:43 -0400

    Camera fixed

--- Scripts/BuildPhaseCamera.cs
+++ Scripts/BuildPhaseCamera.cs
@@ -8,44 +8,32 @@ public class BuildPhaseCamera : MonoBehaviour
     private float x;
     private float y;
     private Vector3 rotateValue;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
         if(Input.GetKey(KeyCode.W))
         {
-            transform.Translate(0, 0, (float)0.1);
+            transform.Translate(0, 0, (float)0.3);
         }
         else if(Input.GetKey(KeyCode.A))
         {
-            transform.Translate((float)-0.1, 0, 0);
+            transform.Translate((float)-0.3, 0, 0);
 
         }
         else if(Input.GetKey(KeyCode.S))
         {
-            transform.Translate(0, 0, (float)-0.1);
+            transform.Translate(0, 0, (float)-0.3);
         }
         else if(Input.GetKey(KeyCode.D))
         {
-            transform.Translate((float)0.1, 0, 0);
+            transform.Translate((float)0.3, 0, 0);
         }
         else if(Input.GetMouseButton(1))
         {
             y = Input.GetAxis("Mouse X") * 3;
             x = Input.GetAxis("Mouse Y") * 3;
-            rotateValue = new Vector3(x, y * -1, 0);
+            rotateValue = new Vector3(x, (float)(y * -1.5), 0);
             transform.eulerAngles = transform.eulerAngles - rotateValue;
         }
-       // yaw += Input.GetAxis("Mouse X") * mouseSensitivity;
-       // pitch -= Input.GetAxis("Mouse Y") * mouseSensitivity;
-
-       // Vector3 targetRotation = new Vector3(pitch, yaw);
-       // transform.eulerAngles = targetRotation;
     }
 }
-////////////////////////////HEY THIS IS JUST ELEMENTARY. ITS BASED OFF OF AN UNFINISHED VERSION OF THE THIRD PERSON CAMERA BUT I THOUGHT ITD BE A USEFUL START FOR WHEN WE GET HERE
--- Scripts/ItemSpawner2.cs
+++ Scripts/ItemSpawner2.cs
@@ -11,9 +11,6 @@ public class ItemSpawner2 : MonoBehaviour
 
     ItemBehavior itemBehavior;
 
-    int xpos = -10;
-    int ypos = 10;
-    int zpos = 90;
 
     void Awake()
     {
@@ -35,9 +32,38 @@ public class ItemSpawner2 : MonoBehaviour
 
     public void AddObject()
     {
+        // Forward vector to see where the camera is facing
+        Vector3 camForward = Camera.main.transform.forward;
+        Vector3 camCoords = Camera.main.transform.position;
+        Vector3 itemCoords = new Vector3(0, 0, 0);
+        print(camForward);
+        // new Vector3(camCoords.x, camCoords.y + 5, camCoords.z + 5);
+        // Case where all coords are +
+        if ((camForward.x >= 0.0 || camForward.x == 0.0) && (camForward.y >= 0.0 || camForward.y == 0) && (camForward.z >= 0.0 || camForward.z == 0.0)) {
+            itemCoords = new Vector3(camCoords.x + 15, camCoords.y, camCoords.z + 10);
+        }
+        // Case where only x is -, y and z +
+        else if (camForward.x < 0 && camForward.y >= 0 && camForward.z >= 0)
+        {
+            itemCoords = new Vector3(camCoords.x - 15, camCoords.y, camCoords.z + 10);
+        }
+        // Case where x and y -, z +
+        else if (camForward.x < 0 && camForward.y < 0 && camForward.z >= 0)
+        {
+            itemCoords = new Vector3(camCoords.x - 15, camCoords.y, camCoords.z + 10);
+        }
+        // Case where all components -
+       else if(camForward.x < 0 && camForward.y < 0 && camForward.z < 0)
+       {
+            itemCoords = new Vector3(camCoords.x - 15, camCoords.y, camCoords.z - 10);
+        }
+        else
+        {
+            itemCoords = new Vector3(camCoords.x + 15, camCoords.y, camCoords.z + 10);
+        }
         if (itemBehavior.quantity > 0)
         {
-            GameObject newSpawn = Instantiate(sampleObject, new Vector3(xpos, ypos, zpos), sampleObject.transform.rotation);
+            GameObject newSpawn = Instantiate(sampleObject, itemCoords, sampleObject.transform.rotation);
             itemBehavior.quantity--;
         }
         else
