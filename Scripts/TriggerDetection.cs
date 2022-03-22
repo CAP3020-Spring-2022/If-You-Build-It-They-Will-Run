72218562b5bae074b7495998d4f2a745c9859428
erinhu00 <58484479+erinhu00@users.noreply.github.com>
2022-03-22 14:30:39 -0400

    Tutorial1

---
+++ Scripts/TriggerDetection.cs
@@ -0,0 +1,23 @@
+﻿// TriggerDetection.cs by Erin Hu
+// Description: Detects the collision and plays "Ding" sound
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerDetection : MonoBehaviour
+{
+    // public AudioClip dingClip;
+    // AudioSource audioSource;
+	    public int levelToLoad;
+
+    void OnTriggerEnter(Collider myCollider){
+        if (myCollider.gameObject.name == "Player"){
+            Debug.Log("You have reached the end of the platform");
+            Application.LoadLevel(levelToLoad);
+        }
+
+        // audioSource = GetComponent<AudioSource>();
+        // audioSource.PlayOneShot(dingClip);
+    }
+}
