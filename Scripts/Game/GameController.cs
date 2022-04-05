7f005bbef6d547e3eb356feb7468be8cbfe881d1
erinhu00 <58484479+erinhu00@users.noreply.github.com>
2022-04-04 20:51:42 -0400

    Menu

--- Scripts/Game/GameController.cs
+++ Scripts/Game/GameController.cs
@@ -7,9 +7,41 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameController : MonoBehaviour {
+    public GameObject menu;
+    public GameObject stambar;
+    public GameObject healthbar;
 
+    public GameObject buildcanvas;
+    void Start()
+    {
+        // menu.SetActive(false);
+    }
 	public void Update () {
         if (Input.GetKey("escape"))
             Application.Quit();
 	}
+
+
+    public void TaskOnClick(){
+        menu.SetActive(true);
+        buildcanvas.SetActive(false);	
+        stambar.SetActive(false);	
+        healthbar.SetActive(false);	
+
+    }
+    public void ExitOnClick(){
+        Application.Quit();	
+    }
+
+    public void camOnClick(){
+        // press.
+    }
+
+    public void ResumeOnClick(){
+        menu.SetActive(false);
+        buildcanvas.SetActive(true);
+        stambar.SetActive(true);	
+        healthbar.SetActive(true);		
+
+    }
 }
